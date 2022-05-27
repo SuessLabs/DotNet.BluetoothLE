0e51bf7882f489b2a6870b844a3b431ebb1b7f41
Damian Suess <djsuess@gmail.com>
2022-05-27 11:25:53 -0400

    StyleCop

--- sandbox/Cross.BluetoothLe/Models/EventArgs/CharacteristicWriteCallbackEventArgs.android.cs
+++ sandbox/Cross.BluetoothLe/Models/EventArgs/CharacteristicWriteCallbackEventArgs.android.cs
@@ -1,17 +1,18 @@
-using System;
+﻿using System;
 using Android.Bluetooth;
 
 namespace System.BluetoothLe.EventArgs
 {
-    public class CharacteristicWriteCallbackEventArgs
-    {
-        public BluetoothGattCharacteristic Characteristic { get; }
-        public Exception Exception { get; }
+  public class CharacteristicWriteCallbackEventArgs
+  {
+    public BluetoothGattCharacteristic Characteristic { get; }
 
-        public CharacteristicWriteCallbackEventArgs(BluetoothGattCharacteristic characteristic, Exception exception = null)
-        {
-            Characteristic = characteristic;
-            Exception = exception;
-        }
+    public Exception Exception { get; }
+
+    public CharacteristicWriteCallbackEventArgs(BluetoothGattCharacteristic characteristic, Exception exception = null)
+    {
+      Characteristic = characteristic;
+      Exception = exception;
     }
+  }
 }
