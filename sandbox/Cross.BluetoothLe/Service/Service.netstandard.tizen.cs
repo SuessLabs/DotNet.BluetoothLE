42ea5849ef13d7f2e6418cb2ccc96dd2630af611
Damian Suess <djsuess@gmail.com>
2022-05-27 10:37:36 -0400

    Base

---
+++ sandbox/Cross.BluetoothLe/Service/Service.netstandard.tizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.BluetoothLe
+{
+    public partial class Service
+    {
+        internal Guid NativeGuid => throw new PlatformNotSupportedException();
+
+        internal bool NativeIsPrimary => throw new PlatformNotSupportedException();
+
+        internal Task<IList<Characteristic>> GetCharacteristicsNativeAsync() => throw new PlatformNotSupportedException();
+
+        internal object NativeService => throw new PlatformNotSupportedException();
+
+        public virtual void Dispose()
+        {
+
+        }
+    }
+}
